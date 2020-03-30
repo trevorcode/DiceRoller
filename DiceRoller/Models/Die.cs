64960ad3503290b711b7c9a396d0d2e9930898a9
Trevor Spencer <trevor@admonex.com>
2020-03-30 17:02:13 -0600

    Created functionality for dice rolling

---
+++ DiceRoller/Models/Die.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiceRoller.Models
+{
+    public class Die : IDie
+    {
+        public string Name { get; set; }
+        public int Sides { get; set; }
+        public int? Result { get; set; }
+
+        public int RollDie()
+        {
+            Random newNumber = new Random();
+            Result = newNumber.Next(1, Sides+1);
+            return Result.Value;
+        }
+    }
+}
