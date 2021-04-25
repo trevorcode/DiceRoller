48b981a1ff7f7a8836e555439ac116468928d96b
Trevor <trevorcode@outlook.com>
2021-04-24 22:01:32 -0600

    Added PWA version and updated to .NET 5

---
+++ DiceRollerPWA/Models/IDie.cs
@@ -0,0 +1,11 @@
+﻿namespace DiceRollerPWA.Models
+{
+    public interface IDie
+    {
+        string Name { get; set; }
+        int Sides { get; set; }
+        int? Result { get; set; }
+
+        int RollDie();
+    }
+}
